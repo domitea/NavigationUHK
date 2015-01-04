c4c81750f6e24cafbe429181c51975e338ba71c6
Dominik Matoulek <domitea@gmail.com>
2015-01-04 11:28:36 +0100

    Creating WebViewInterface Class

--- app/src/main/java/uhk/kikm/navigationuhk/utils/WebViewInterface.java
+++ app/src/main/java/uhk/kikm/navigationuhk/utils/WebViewInterface.java
@@ -1,6 +1,7 @@
 package uhk.kikm.navigationuhk.utils;
 
 import android.content.Context;
+import android.webkit.JavascriptInterface;
 
 /** Trida reprezetntujici interface k WebView
  *
@@ -9,9 +10,37 @@ public class WebViewInterface {
 
     Context context;
 
+    int x;
+    int y;
+
+    boolean changed = false;
+
     public WebViewInterface(Context context) {
         this.context = context;
     }
 
+    @JavascriptInterface
+    private void setXY(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
 
+        changed = true;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isChanged() {
+        return changed;
+    }
+
+    public void setChanged(boolean changed) {
+        this.changed = changed;
+    }
 }
