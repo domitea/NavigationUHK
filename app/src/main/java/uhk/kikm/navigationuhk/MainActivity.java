2ab78d93d00c21b3e2be7b8f87208f0e7d0a0302
Dominik Matoulek <domitea@gmail.com>
2015-01-04 10:51:37 +0100

    Init commit

---
+++ app/src/main/java/uhk/kikm/navigationuhk/MainActivity.java
@@ -0,0 +1,80 @@
+package uhk.kikm.navigationuhk;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = (WebView) findViewById(R.id.WebView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.getSettings().setBuiltInZoomControls(true);
+        view.getSettings().setSupportZoom(true);
+        view.setWebViewClient(new WebViewClient());
+        view.loadData(readTextFromResource(R.drawable.uhk_j_2_level), null, "UTF-8");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Metoda na nacateni textu z nejakeho souboru.
+     * @param resourceID ID zdroje
+     * @return String text
+     */
+    private String readTextFromResource(int resourceID)
+    {
+        InputStream raw = getResources().openRawResource(resourceID);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        int i;
+        try
+        {
+            i = raw.read();
+            while (i != -1)
+            {
+                stream.write(i);
+                i = raw.read();
+            }
+            raw.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return stream.toString();
+
+    }
+}
---
+++ app/src/main/java/uhk/kikm/navigationuhk/utils/WebViewInterface.java
@@ -0,0 +1,17 @@
+package uhk.kikm.navigationuhk.utils;
+
+import android.content.Context;
+
+/** Trida reprezetntujici interface k WebView
+ *
+ */
+public class WebViewInterface {
+
+    Context context;
+
+    public WebViewInterface(Context context) {
+        this.context = context;
+    }
+
+
+}
