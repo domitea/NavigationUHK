dfb77638e6578ec78621a530dd52fef484a20c0d
Dominik Matoulek <domitea@gmail.com>
2015-02-05 09:30:48 +0100

    Making CouchDB support

--- app/src/main/java/uhk/kikm/navigationuhk/model/couchDBManager.java
+++ app/src/main/java/uhk/kikm/navigationuhk/model/couchDBManager.java
@@ -2,6 +2,8 @@ package uhk.kikm.navigationuhk.model;
 
 import android.content.Context;
 
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.android.AndroidContext;
 
@@ -13,15 +15,22 @@ import java.io.IOException;
 public class couchDBManager {
     Context context;
     Manager manager;
+    Database db;
 
     public couchDBManager(Context context) {
         this.context = context;
         try {
             manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
+
+            this.db = manager.getDatabase("ScanUHK");
         }
         catch (IOException e)
         {
             e.printStackTrace();
+        }
+        catch (CouchbaseLiteException cle)
+        {
+            cle.printStackTrace();
         }
     }
 }
