df30b7dc70338f7dc87f4020e141e258faf436a4
Dominik Matoulek <domitea@gmail.com>
2015-02-07 14:15:43 +0100

    Making WifiFinder

--- app/src/main/java/uhk/kikm/navigationuhk/utils/WifiFinder.java
+++ app/src/main/java/uhk/kikm/navigationuhk/utils/WifiFinder.java
@@ -14,14 +14,14 @@ import uhk.kikm.navigationuhk.model.Scan;
  */
 public class WifiFinder {
     private ArrayList<Position> positions;
-    private HashMap<String, ArrayList<Scan>> navigationData;
+    private HashMap<String, Position> navigationData;
     private HashMap<Scan, Position> positionsOfScans;
 
     public WifiFinder(ArrayList<Position> positions) {
         this.positions = positions;
         for (Position p : positions) // pro vsechny polohy patre
         {
-            navigationData.put(String.valueOf(p.getX()) + String.valueOf(p.getY()), p.getScans()); // pridej do seznamu vsechny scany s hasem polohy
+            navigationData.put(String.valueOf(p.getX()) + String.valueOf(p.getY()), p); // pridej do seznamu vsechny scany s hasem polohy
             for (Scan s : p.getScans()) {
                 // Kazdy sken patri k urcite poloze... Kazdy sken je take jedinecny, hodnoty muzou byt stejne, ale je jedinencny - je to rychlejsi, nez proheledavani cyklem
                 positionsOfScans.put(s, p);
@@ -37,24 +37,39 @@ public class WifiFinder {
         this.positions = positions;
     }
 
-    public Position getPosition(List<ScanResult> scanForIdentify) {
+    public Position getPosition(List<ScanResult> scansForIdentify) {
+
+        float distance = 0;
 
         for (Position p : positions)
         {
-            for (ScanResult s : scanForIdentify)
+            for (ScanResult s : scansForIdentify)
             {
+                int index = containsMAC(s,p);
 
+                if (index >= 0)
+                {
+                    distance += Math.pow(s.level - p.getScan(index).getStrenght(), 2);
+                }
+                else if (index == -1)
+                {
+                    distance += Math.pow(0 - p.getScan(index).getStrenght(), 2);
+                }
             }
+
+            distance = (float) Math.sqrt(distance);
+
+
         }
 
         return new Position();
     }
 
-    private int containsMAC(Scan s, Position p)
+    private int containsMAC(ScanResult s, Position p)
     {
         for(int i = 0; i < p.getScans().size(); i++)
         {
-            if (s.getMAC().equals(p.getScan(i).getMAC()))
+            if (s.BSSID.equals(p.getScan(i).getMAC()))
             {
                 return i;
             }
