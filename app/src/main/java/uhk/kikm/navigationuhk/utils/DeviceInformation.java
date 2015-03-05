15f0637c0f0e1da78c7da2e3e38e37f20d1d0452
Dominik Matoulek <domitea@gmail.com>
2015-03-05 23:12:06 +0100

    Adding sensor support

---
+++ app/src/main/java/uhk/kikm/navigationuhk/utils/DeviceInformation.java
@@ -0,0 +1,14 @@
+package uhk.kikm.navigationuhk.utils;
+
+import android.content.Context;
+
+/**
+ * Created by dominik on 5.3.15.
+ */
+public class DeviceInformation {
+
+
+    public DeviceInformation(Context context) {
+        context.getSystemService()
+    }
+}
