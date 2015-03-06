78da9502459a3d403f023c6087926bfda53e47be
Dominik Matoulek <domitea@gmail.com>
2015-03-06 09:17:20 +0100

    Adding sensor support

--- app/src/main/java/uhk/kikm/navigationuhk/utils/DeviceInformation.java
+++ app/src/main/java/uhk/kikm/navigationuhk/utils/DeviceInformation.java
@@ -1,14 +1,19 @@
 package uhk.kikm.navigationuhk.utils;
 
 import android.content.Context;
+import android.os.Build;
+import android.telephony.TelephonyManager;
 
 /**
  * Created by dominik on 5.3.15.
  */
 public class DeviceInformation {
 
+    TelephonyManager telephonyManager;
+    Build build;
 
     public DeviceInformation(Context context) {
-        context.getSystemService()
+        build = new Build();
+        telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     }
 }
