540a3efe5b4c4edfb48daa56c29180789366a180
Dominik Matoulek <domitea@gmail.com>
2015-03-06 09:57:26 +0100

    Adding sensor support

--- app/src/main/java/uhk/kikm/navigationuhk/model/Position.java
+++ app/src/main/java/uhk/kikm/navigationuhk/model/Position.java
@@ -18,6 +18,12 @@ public class Position {
     String description;
     ArrayList<Scan> scans;
 
+    // other recorded stuff...
+    private float accX, accY, accZ, gyroX, gyroY, gyroZ, magX, magY, magZ;
+    private String board, bootloader, brand, device, display, fingerprint, hardware, host, osId, manufacturer, model, product, serial, tags, type, user;
+
+    private String[] Supported_ABIS;
+
     Date createdDate;
 
     public Position() {
@@ -86,5 +92,213 @@ public class Position {
     public Scan getScan(int index)
     {
         return scans.get(index);
+    }
+
+    public float getAccX() {
+        return accX;
+    }
+
+    public void setAccX(float accX) {
+        this.accX = accX;
+    }
+
+    public float getAccY() {
+        return accY;
+    }
+
+    public void setAccY(float accY) {
+        this.accY = accY;
+    }
+
+    public float getAccZ() {
+        return accZ;
+    }
+
+    public void setAccZ(float accZ) {
+        this.accZ = accZ;
+    }
+
+    public float getGyroX() {
+        return gyroX;
+    }
+
+    public void setGyroX(float gyroX) {
+        this.gyroX = gyroX;
+    }
+
+    public float getGyroY() {
+        return gyroY;
+    }
+
+    public void setGyroY(float gyroY) {
+        this.gyroY = gyroY;
+    }
+
+    public float getGyroZ() {
+        return gyroZ;
+    }
+
+    public void setGyroZ(float gyroZ) {
+        this.gyroZ = gyroZ;
+    }
+
+    public float getMagX() {
+        return magX;
+    }
+
+    public void setMagX(float magX) {
+        this.magX = magX;
+    }
+
+    public float getMagY() {
+        return magY;
+    }
+
+    public void setMagY(float magY) {
+        this.magY = magY;
+    }
+
+    public float getMagZ() {
+        return magZ;
+    }
+
+    public void setMagZ(float magZ) {
+        this.magZ = magZ;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+    public String getBootloader() {
+        return bootloader;
+    }
+
+    public void setBootloader(String bootloader) {
+        this.bootloader = bootloader;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public String getFingerprint() {
+        return fingerprint;
+    }
+
+    public void setFingerprint(String fingerprint) {
+        this.fingerprint = fingerprint;
+    }
+
+    public String getHardware() {
+        return hardware;
+    }
+
+    public void setHardware(String hardware) {
+        this.hardware = hardware;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getOsId() {
+        return osId;
+    }
+
+    public void setOsId(String osId) {
+        this.osId = osId;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String[] getSupported_ABIS() {
+        return Supported_ABIS;
+    }
+
+    public void setSupported_ABIS(String[] supported_ABIS) {
+        Supported_ABIS = supported_ABIS;
     }
 }
--- app/src/main/java/uhk/kikm/navigationuhk/utils/DeviceInformation.java
+++ app/src/main/java/uhk/kikm/navigationuhk/utils/DeviceInformation.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Build;
 import android.telephony.TelephonyManager;
 
+import uhk.kikm.navigationuhk.model.Position;
+
 /**
  * Created by dominik on 5.3.15.
  */
@@ -15,5 +17,10 @@ public class DeviceInformation {
     public DeviceInformation(Context context) {
         build = new Build();
         telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    }
+
+    public Position fillPosition(Position p)
+    {
+        return p;
     }
 }
