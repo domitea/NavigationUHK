e4d2dd4595bacc6130de229927fe553e92d9d699
Dominik Matoulek <domitea@gmail.com>
2015-03-07 17:23:32 +0100

    Adding sensor support

--- app/src/main/java/uhk/kikm/navigationuhk/PositionInfoActivity.java
+++ app/src/main/java/uhk/kikm/navigationuhk/PositionInfoActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import uhk.kikm.navigationuhk.model.CouchDBManager;
@@ -29,6 +31,14 @@ public class PositionInfoActivity extends ActionBarActivity {
 
         textView.setText(position.toString());
 
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
