fcf341ffa2d4626703fc8331efdc6bc68dd6785c
Dominik Matoulek <domitea@gmail.com>
2015-03-08 11:05:24 +0100

    Adding sensor support

---
+++ app/src/main/java/uhk/kikm/navigationuhk/SettingsFactory.java
@@ -0,0 +1,11 @@
+package uhk.kikm.navigationuhk;
+
+import uhk.kikm.navigationuhk.utils.LocalizationService.LocalizationServicePoint;
+
+/**
+ * Created by dominik on 8.3.15.
+ */
+public class SettingsFactory {
+    public static LocalizationServicePoint pointA = new LocalizationServicePoint(34,31,new Float(50.2045875),new Float(15.8290822));
+    public static LocalizationServicePoint pointB = new LocalizationServicePoint(642,39,new Float(50.2045247),new Float(15.8297047));
+}
