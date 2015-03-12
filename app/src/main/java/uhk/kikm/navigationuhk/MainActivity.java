74655b1dafe74d099c20b76d21864f8288f734c4
Dominik Matoulek <domitea@gmail.com>
2015-03-12 10:15:59 +0100

    Adding sensor support

--- app/src/main/java/uhk/kikm/navigationuhk/MainActivity.java
+++ app/src/main/java/uhk/kikm/navigationuhk/MainActivity.java
@@ -142,6 +142,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
         return true;
     }
 
